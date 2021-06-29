acfabb584868b386f5fc288cf9714fbcda859244
陈亮 <chenliang_google@163.com>
2021-06-29 11:44:01 +0800

    优化ViewModel接口调用，简化调用

--- app/src/main/java/com/chenliang/vm/TestViewModel.kt
+++ app/src/main/java/com/chenliang/vm/TestViewModel.kt
@@ -7,14 +7,8 @@ import com.chenliang.net.go
 
 class TestViewModel : ViewModel() {
 
-    /**
-     * 获取Object数据测试，必须返回 MutableLiveData<BaseResponse<T>>类型
-     */
     fun test(con: Context, name: String) = go { API.getData("tom", "23") }
 
-    /**
-     * 获取Array数据测试，必须返回 MutableLiveData<BaseResponse<ArrayList<T>>>类型
-     */
     fun tests(con: Context, name: String) = go { API.getDatas("tom", "23") }
 
 }
